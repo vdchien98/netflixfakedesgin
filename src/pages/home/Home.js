c7d265442c9de53b08b555be7e4aa953dbb0111c
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-08-25 23:01:14 +0700

    unoptimized get(`http://localhost:9999/api/lists) in Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Home.scss';
 // import { AcUnit } from '@material-ui/icons';
 import NavBar from '../../componets/NavBar/NavBar';
 import Featured from '../../componets/featured/Featured';
 import List from '../../componets/list/List';
+import axios from 'axios';
 
 const Home = ({ type }) => {
+    const [lists, setLists] = useState([]);
+    const [genre, setGenre] = useState(null);
+    useEffect(() => {
+        const getRamdomLists = async () => {
+            try {
+                const res = await axios.get(`http://localhost:9999/api/lists${type ? '?type=' + type : ''}${genre ? '&genre=' + genre : ''}`, {
+                    headers: {
+                        token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDQ2MWU2ODAzNTM3NDU3YmYxNDU0YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTQ0MTY0NSwiZXhwIjoxNjYxODczNjQ1fQ.GmkgJLCqlqmrAWK_7uhiXtyMBwG_F5xC5MdB56NdWTo',
+                    },
+                });
+                console.log(res);
+                setLists(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getRamdomLists();
+    }, [type, genre]);
     return (
         <div className="home">
             <NavBar />
-            <Featured type={type} />
-            <List />
-            <List />
-            <List />
-            <List />
-            <List />
+            <Featured type={type} setGenre={setGenre} />
+            {lists.map((list) => (
+                <List list={list} />
+            ))}
         </div>
     );
 };
